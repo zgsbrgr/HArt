effc5bdb5ac135de5455046ff31df23a6f59b690
gerg <gergely.zalai@arworks.com>
2021-10-06 17:17:06 +0200

    result wrapper extended with loading statuses

--- core/src/main/java/com/zgsbrgr/demos/hart/core/domain/Result.kt
+++ core/src/main/java/com/zgsbrgr/demos/hart/core/domain/Result.kt
@@ -9,15 +9,21 @@ import java.lang.Exception
 sealed class Result<out R> {
     data class Success<out T>(val data: T) : Result<T>()
     data class Error(val exception: Exception): Result<Nothing>()
-    object Loading: Result<Nothing>()
+    data class Loading<T>(val state: LoadingState = LoadingState.Idle): Result<T>()
 
     override fun toString(): String {
         return when (this) {
             is Success<*> -> "Success[data=$data]"
             is Error -> "Error[exception=$exception]"
-            Loading -> "Loading"
+            is Loading -> "Loading[state=$state]"
         }
     }
+
+}
+
+sealed class LoadingState {
+    object Loading: LoadingState()
+    object Idle: LoadingState()
 }
 
 /**

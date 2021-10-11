bf637c00fdb000bdd05faddffb48c26a04dd7873
gerg <gergely.zalai@arworks.com>
2021-10-11 13:54:16 +0200

    image loader module for coil

---
+++ app/src/main/java/com/zgsbrgr/demos/hart/di/CoilModule.kt
@@ -0,0 +1,28 @@
+package com.zgsbrgr.demos.hart.di
+
+import android.app.Application
+import coil.ImageLoader
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoilModule {
+
+    @Provides
+    @Singleton
+    fun provideImageLoader(app: Application): ImageLoader {
+        return ImageLoader.Builder(app)
+            .error(android.R.drawable.stat_notify_error)
+            .placeholder(android.R.drawable.alert_light_frame)
+            .availableMemoryPercentage(0.25)
+            .crossfade(true)
+            .build()
+    }
+
+}
